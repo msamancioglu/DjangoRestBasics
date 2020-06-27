8093b0e81d1df08a7697233c2bd01017aed776e6
Mustafa Samancioglu <msamancioglu@hotmail.com>
2020-06-28 00:24:55 +0300

    Added APIView for GET and POST methods

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import article_list, article_detail, ArticleAPIView
 #from rest_framework.schemas import get_schema_view
 
 from rest_framework.documentation import include_docs_urls
@@ -10,7 +10,9 @@ from rest_framework.documentation import include_docs_urls
 #schema_view = get_schema_view(title='PasteBin Schema View')
 
 urlpatterns = [
-    path('article/', article_list),
+    #path('article/', article_list),
+    path('article/', ArticleAPIView.as_view()),
+
     path('article/<int:pk>/', article_detail),
     #path('schemas/', schema_view),
     path('api-docs/', include_docs_urls(title='Rest API', description="All api details here")),
--- api_basic/views.py
+++ api_basic/views.py
@@ -15,7 +15,24 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+
+from rest_framework.views import APIView
 # Create your views here.
+
+#Class based api view
+class ArticleAPIView(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
 
 
 @api_view(['GET', 'POST'])
