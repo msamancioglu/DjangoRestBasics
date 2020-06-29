b396cbc2e678cdb129b7bf34038840b57a303409
Mustafa Samancioglu <msamancioglu@hotmail.com>
2020-06-29 15:30:58 +0300

    added token serializer

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import Article
+from rest_framework.authtoken.models import Token
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         #fields = ['id', 'title', 'author','email', 'date']
         fields = '__all__'
+
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = '__all__'
+
